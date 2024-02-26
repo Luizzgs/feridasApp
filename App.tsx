272205e4e308658ad965c82d6ea8b7e56ed3d7dd
Luizzgs <lgsilvakies@gmail.com>
2024-02-26 12:17:04 -0300

    abre a camera

--- App.tsx
+++ App.tsx
@@ -1,118 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useEffect, useState } from 'react';
+import {ActivityIndicator, Button, GestureResponderEvent, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { useCameraPermission, useCameraDevice, Camera } from 'react-native-vision-camera';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const CameraApp = () => {
+  const {hasPermission, requestPermission} = useCameraPermission();
+  const device = useCameraDevice('back');
+  const [isCameraOpen, setCameraOpen] = useState<boolean>(false);
+  
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }
+  , [hasPermission]);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  console.log('hasPermission', hasPermission);
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  if (device == null) return <ActivityIndicator />;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  if(!hasPermission){
+    return <Text>No permission</Text>
+  }
+    
+  if(!device){
+    return <Text>No device</Text>
+  }
+
+ 
+  const openCamera = async () => {
+    try {
+      // Inicializar a câmera
+           // Abrir a câmera
+      await Camera;
+      setCameraOpen(true);
+    } catch (error) {
+      console.error('Erro ao abrir a câmera:', error);
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  return (    
+    <View style={styles.container}>
+      {!isCameraOpen ? (
+        <View style={styles.content}>
+          <Text>Pressione o botão para abrir a câmera</Text>
+          <TouchableOpacity onPress={openCamera} style={styles.button}>
+            <Text style={styles.buttonText}>Abrir Câmera</Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      ) : (
+        <Camera device={device} style={StyleSheet.absoluteFill} isActive={true}>          
+        </Camera>       
+        
+        
+      )}
+    </View>
   );
-}
+};
+    
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: 'blue',
+    borderRadius: 5,
   },
-  highlight: {
-    fontWeight: '700',
+  buttonText: {
+    color: 'white',
   },
 });
 
-export default App;
+
+export default CameraApp;
+
+
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function CameraApp() {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+}
+
